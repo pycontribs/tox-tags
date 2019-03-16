c9968c0de98b6a2e74daf5ea124b657d97210bc5
Sorin Sbarnea <ssbarnea@redhat.com>
2019-03-16 09:44:31 +0000

    Add packaging to tox-tags
    Makes tox-tags publishable on pypi.

--- setup.py
+++ setup.py
@@ -1,23 +1,6 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import setuptools
 
-from setuptools import setup
-
-setup(
-    name='tox-tags',
-    url='https://github.com/AndreLouisCaron/tox-tags',
-    description='Adds tags for running subsets of Tox environments.',
-    maintainer='Andre Caron',
-    maintainer_email='andre.l.caron@gmail.com',
-    version='0.1.0',
-    py_modules=[
-        'tox_tags',
-    ],
-    entry_points={
-        'tox': [
-            'tags = tox_tags',
-        ],
-    },
-    install_requires=[
-        'tox>=2.0',
-    ],
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
